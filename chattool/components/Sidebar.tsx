a8680146d22a7e628e6ead9fb612e23e0c8b4fe3
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-02-12 22:38:07 +0530

    chatrow function created implemented to the sidebar

--- chattool/components/Sidebar.tsx
+++ chattool/components/Sidebar.tsx
@@ -4,26 +4,30 @@ import { use } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { PlusIcon } from "@radix-ui/react-icons";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import ChatRow from "./ChatRow"
 
 function Sidebar() {
     const router = useRouter();
     const { closeMobileNav, isMobileNavOpen } = use(NavigationContext);
-
+    const chats = useQuery(api.chats.listChats);
     const createChat = useMutation(api.chats.createChat);
-
-    const handleClick = () => {
-        //TODO: Route
-        //router.push("/dashboard/chat");
-        closeMobileNav();
-    };
+    const deleteChat = useMutation(api.chats.deleteChat);
 
     const handleNewChat = async () => {
         const chatId = await createChat({ title: "New Chat" });
         router.push(`/dashboard/chat/${chatId}`);
         closeMobileNav();
       };
+
+    const handleDeleteChat = async (id: Id<"chats">) => {
+        await deleteChat({ id });
+        if(window.location.pathname.includes(id)) {
+            router.push("/dashboard");
+        }
+    };
 
 
 
@@ -54,9 +58,9 @@ function Sidebar() {
         </div>
 
         <div className="flex-1 overflow-y-auto space-y-2.5 p-4 scrollbar-thin scrollbar-thumb-gray-200 scrollbar-track-transparent">
-          {/* {chats?.map((chat) => (
+          {chats?.map((chat) => (
             <ChatRow key={chat._id} chat={chat} onDelete={handleDeleteChat} />
-          ))} */}
+          ))}
         </div>
       </div>
 
