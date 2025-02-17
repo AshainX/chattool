f0e0b44a07d3b5a5755f94e725eed857df0186f7
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-02-17 23:08:23 +0530

    twonineteenzeroseven

---
+++ chattool/app/dashboard/chat/[chatId]/page.tsx
@@ -0,0 +1,29 @@
+import { Id } from "@/convex/_generated/dataModel";
+import { getConvexClient } from "@/lib/convex";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { api } from "@/convex/_generated/api";
+
+interface ChatPageProps {
+    params: Promise<{
+        chatId: Id<"chats">
+    }>;
+}
+
+
+
+async function ChatPage({params}: ChatPageProps) {
+    const { chatId } = await params;
+    const { userId } = await auth(); //user authentication
+
+    if(!userId){
+        redirect("/");
+    }
+    const convex = getConvexClient();
+
+    const initialMessages = await convex.query(api.nessages.list, {chatId});
+
+    return<div>ChatPage : {chatId} </div>
+}
+
+export default ChatPage
