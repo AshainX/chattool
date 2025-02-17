89d8efb6ffb4642c2ce23a73b9c15a0f9f3fdc25
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-02-17 22:32:21 +0530

    added delete and adding

--- chattool/components/ChatRow.tsx
+++ chattool/components/ChatRow.tsx
@@ -2,6 +2,8 @@ import { Doc, Id } from "@/convex/_generated/dataModel";
 import { NavigationContext } from "@/lib/NavigationProvider";
 import { useRouter } from "next/navigation";
 import { use } from "react";
+import { Button } from "@/components/ui/button";
+import { PlusIcon, TrashIcon } from "@radix-ui/react-icons";
 
 function ChatRow({
     chat,
@@ -19,10 +21,32 @@ function ChatRow({
     };
 
     return (<div
-    className="group rounded-xl border border-gray-200/30 bg-white/50 backdrop-blur-sm hover:bg-white/80 
-    transition-all duration-200 cursor-pointer shadow-sm hover:shadow-md"
+    className="group rounded-xl border border-gray-200/30 bg-white/50 backdrop-blur-sm hover:bg-white/80 transition-all duration-200 cursor-pointer shadow-sm hover:shadow-md"
+    onClick={handleClick}
     >
-        ChatRow
+    <div className="p-4">
+      <div className="flex justify-between items-start">Chat
+
+      <Button
+            variant="ghost"
+            size="icon"
+            className="opacity-0 group-hover:opacity-100 -mr-2 -mt-2 ml-2 transition-opacity duration-200"
+            onClick={(e) => {
+              e.stopPropagation();
+              onDelete(chat._id);
+            }}
+          >
+            <TrashIcon className="h-4 w-4 text-gray-400 hover:text-red-500 transition-colors" />
+          </Button>
+      </div>
+
+      {/* LastMessage */}
+        {/* {lastMessage && (
+          <p className="test-xs text-gray-400 mt-1.5 font-medium">
+            <TimeAgo date={lastMessage.createdAt} />
+          </p>
+        )} */}
+    </div>
     </div>
     );
   }
